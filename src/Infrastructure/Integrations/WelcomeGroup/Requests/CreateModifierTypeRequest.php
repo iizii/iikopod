d1c7539344169399f006beb3111d73a9697165c7
vlad-grivenko <vlad-grivenko@amazingcat.net>
2024-11-07 11:42:01 +0300

    Добавил реквесты, дто, ентити, респонс даты для ModifierType в ПОД, а также запуск Pint

---
+++ src/Infrastructure/Integrations/WelcomeGroup/Requests/CreateModifierTypeRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infrastructure\Integrations\WelcomeGroup\Requests;
+
+use Illuminate\Http\Client\Response;
+use Infrastructure\Integrations\WelcomeGroup\DataTransferObjects\Food\CreateFoodRequestData;
+use Infrastructure\Integrations\WelcomeGroup\DataTransferObjects\Food\CreateFoodResponseData;
+use Shared\Infrastructure\Integrations\RequestInterface;
+use Shared\Infrastructure\Integrations\RequestMethod;
+use Shared\Infrastructure\Integrations\ResponseDataInterface;
+
+final readonly class CreateModifierTypeRequest implements RequestInterface, ResponseDataInterface
+{
+    public function __construct(private CreateFoodRequestData $data) {}
+
+    public function method(): RequestMethod
+    {
+        return RequestMethod::POST;
+    }
+
+    public function endpoint(): string
+    {
+        return '/api/modifier_type';
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function data(): array
+    {
+        return $this->data->toArray();
+    }
+
+    public function createDtoFromResponse(Response $response): CreateFoodResponseData
+    {
+        return CreateFoodResponseData::from($response->json());
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function headers(): array
+    {
+        return [];
+    }
+}
