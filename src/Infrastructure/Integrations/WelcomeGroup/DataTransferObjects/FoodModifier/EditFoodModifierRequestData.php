ec76342833b91130ab545186dd9906b9b586a928
vlad-grivenko <vlad-grivenko@amazingcat.net>
2024-11-07 19:52:05 +0300

    Добавлены реквесты для FoodModifier (получение, редактирование)

---
+++ src/Infrastructure/Integrations/WelcomeGroup/DataTransferObjects/FoodModifier/EditFoodModifierRequestData.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infrastructure\Integrations\WelcomeGroup\DataTransferObjects\FoodModifier;
+
+use Shared\Infrastructure\Integrations\ResponseData;
+
+final class EditFoodModifierRequestData extends ResponseData
+{
+    public function __construct(
+        public readonly int $food, //id
+        public readonly int $modifier, //id
+        public readonly int $weight,
+        public readonly int $caloricity,
+        public readonly float $price,
+        public readonly int $duration,
+    ) {}
+}
