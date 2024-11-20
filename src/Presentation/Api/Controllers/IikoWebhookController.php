2bbb3ee6b01b9ed3a3076d26dcf2bf7812b1c20e
undirezt <kodeksoff@gmail.com>
2024-11-20 20:25:54 +0300

    дополнительно логгирование

--- src/Presentation/Api/Controllers/IikoWebhookController.php
+++ src/Presentation/Api/Controllers/IikoWebhookController.php
@@ -9,13 +9,17 @@ use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Presentation\Api\Requests\IikoWebhookRequest;
+use Shared\Presentation\Middleware\ContextualLogMiddleware;
 use Spatie\RouteAttributes\Attributes\Route;
 
 final readonly class IikoWebhookController
 {
-    public function __construct(private ResponseFactory $responseFactory, private WebhookEventFactory $webhookEventFactory) {}
+    public function __construct(
+        private ResponseFactory $responseFactory,
+        private WebhookEventFactory $webhookEventFactory,
+    ) {}
 
-    #[Route(methods: 'POST', uri: '/iiko/webhook', name: 'iiko.webhook')]
+    #[Route(methods: 'POST', uri: '/iiko/webhook', name: 'iiko.webhook', middleware: [ContextualLogMiddleware::class])]
     public function __invoke(Request $request): JsonResponse
     {
         $this->webhookEventFactory->fromEventCollection(IikoWebhookRequest::collect($request->all()));
