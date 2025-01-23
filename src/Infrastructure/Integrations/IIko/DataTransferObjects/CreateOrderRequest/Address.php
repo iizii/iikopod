d47d2544a7c89dc3e082a045554262d5ddadf533
vlad-grivenko <vlad-grivenko@amazingcat.net>
2025-01-23 08:48:31 +0300

    hotfix. Добавил nullable параметр для св-ва line1 получаемого и отправляемого в IIKO

--- src/Infrastructure/Integrations/IIko/DataTransferObjects/CreateOrderRequest/Address.php
+++ src/Infrastructure/Integrations/IIko/DataTransferObjects/CreateOrderRequest/Address.php
@@ -11,7 +11,7 @@ use Spatie\LaravelData\Data;
 final class Address extends Data
 {
     public function __construct(
-        public readonly string $line1,
+        public readonly ?string $line1,
         public readonly string $flat,
         public readonly string $entrance,
         public readonly string $floor,
