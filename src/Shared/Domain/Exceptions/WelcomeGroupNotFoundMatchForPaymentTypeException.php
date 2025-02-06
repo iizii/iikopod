a09093c79c41daacb153d4c13de34cce03ffb8a2
vlad-grivenko <vlad-grivenko@amazingcat.net>
2025-02-06 06:54:47 +0300

    hotfix. pint

--- src/Shared/Domain/Exceptions/WelcomeGroupNotFoundMatchForPaymentTypeException.php
+++ src/Shared/Domain/Exceptions/WelcomeGroupNotFoundMatchForPaymentTypeException.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 namespace Shared\Domain\Exceptions;
 
 use Exception;
+
 final class WelcomeGroupNotFoundMatchForPaymentTypeException extends Exception implements ShouldNotifyOperator {}
--- src/Shared/Domain/Exceptions/WelcomeGroupOrderItemsNotFoundForOrderException.php
+++ src/Shared/Domain/Exceptions/WelcomeGroupOrderItemsNotFoundForOrderException.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 namespace Shared\Domain\Exceptions;
 
 use Exception;
+
 final class WelcomeGroupOrderItemsNotFoundForOrderException extends Exception implements ShouldNotifyOperator {}
