866b481e7b2bd6906e281eb3be6fb3dc6a359083
iizii <iizii89@gmail.com>
2025-06-23 12:22:57 +0400

    Add tests for WebhookEventDataFactory and rename exception

--- src/Application/Iiko/Factories/WebhookEventDataFactory.php
+++ src/Application/Iiko/Factories/WebhookEventDataFactory.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Application\Iiko\Factories;
 
 use Domain\Iiko\Enums\WebhookEventType;
-use Domain\Iiko\Exceptions\IikoEventTypeNotFountException;
+use Domain\Iiko\Exceptions\IikoEventTypeNotFoundException;
 use Presentation\Api\DataTransferObjects;
 use Presentation\Api\Requests\IikoWebhookRequest;
 use Spatie\LaravelData\Data;
@@ -17,7 +17,7 @@ final readonly class WebhookEventDataFactory
         return match ($request->eventType->value) {
             WebhookEventType::DELIVERY_ORDER_UPDATE->value => DataTransferObjects\DeliveryOrderUpdateData\EventData::from($request->eventInfo),
             WebhookEventType::STOP_LIST_UPDATE->value => DataTransferObjects\StopListUpdateData\EventData::from(['organizationId' => $request->organizationId, 'items' => $request->eventInfo['terminalGroupsStopListsUpdates']]),
-            default => throw new IikoEventTypeNotFountException(),
+            default => throw new IikoEventTypeNotFoundException(),
         };
     }
 }
